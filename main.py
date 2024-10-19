1264d1089f00237ae77c955c70d8b73c7af48371
Arshad shemil k <160718560+Arshadshemilk@users.noreply.github.com>
2024-10-19 06:44:05 +0000

    edit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import dotenv_values
 import streamlit as st
 from groq import Groq
+import time
 
 
 def parse_groq_stream(stream):
@@ -10,7 +11,11 @@ def parse_groq_stream(stream):
             if chunk.choices[0].delta.content is not None:
                 yield chunk.choices[0].delta.content
 
-
+def f_data(response):
+    for word in response.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+        
 # streamlit page configuration
 st.set_page_config(
     page_title="Iron-llama",
@@ -81,6 +86,7 @@ if user_prompt:
             messages=messages,
             stream=True  # for streaming the message
         )
-        response = st.write_stream(parse_groq_stream(stream))
+        response = parse_groq_stream(stream)
+        st.write_stream(f_data(response))
     st.session_state.chat_history.append(
         {"role": "assistant", "content": response})
